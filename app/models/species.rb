204edfe3e2f6ff7621518cb779b5fd46aaa58c25
Kurt Pili <kurtpili3@gmail.com>
2022-03-30 22:07:29 -0500

    Model(species): Added validation and migrated model for testing phase next.

--- app/models/species.rb
+++ app/models/species.rb
@@ -1,2 +1,7 @@
 class Species < ApplicationRecord
+  # Association
+  # has_many :inventory_details
+
+  # Validation
+  validates :name, presence: true, uniqueness: true, length: { is: 7 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_31_024048) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_31_025920) do
   create_table "grind_types", force: :cascade do |t|
     t.string "grind"
     t.datetime "created_at", null: false
@@ -22,6 +22,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_31_024048) do
     t.text "job_desc"
     t.date "start_date"
     t.date "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "species", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

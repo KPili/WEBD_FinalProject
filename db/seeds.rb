dbe8fd225d2621972c3947716f5511798d406595
Kurt Pili <kurtpili3@gmail.com>
2022-03-30 22:11:54 -0500

    File(Seeds.rb): Added table resetting scripts for models that have passed testing phases - Species

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,13 @@
 # Reset Database
 Job.destroy_all
 GrindType.destroy_all
+Species.destroy_all
 
 puts "*** Table Contents Deleted ***"
 
 # Reset PK to 1 for all tables
 Job.connection.execute("UPDATE SQLITE_SEQUENCE SET SEQ=0 WHERE NAME= 'jobs")
 GrindType.connection.execute("UPDATE SQLITE_SEQUENCE SET SEQ=0 WHERE NAME= 'grind_types")
+Species.connection.execute("UPDATE SQLITE_SEQUENCE SET SEQ=0 WHERE NAME= 'species'")
 
 puts "*** Primary Key auto-incrementing value reset to 1 ***"

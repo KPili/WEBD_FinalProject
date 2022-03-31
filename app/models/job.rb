09107eafcf2fda04db27d1fb787c49873f61d12b
Kurt Pili <kurtpili3@gmail.com>
2022-03-30 20:57:07 -0500

    Model(Job): Added Validation, and comments for associations to be included later on.

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,9 @@
 class Job < ApplicationRecord
+  # Association
+  # has_many :employees
+  # has_many :suppliers
+
+  # Validation
+  validates :job_title, :job_desc, :start_date, presence: true
+  validates :end_date, comparison: { greater_than_or_equal_to: :start_date }, allow_blank: true
 end

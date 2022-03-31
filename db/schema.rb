5d5fdba5dceaa73046760d98eadd46a8c2e8f4be
Kurt Pili <kurtpili3@gmail.com>
2022-03-31 01:32:09 -0500

    Model(inventory): Migration initiated, schema.rb file changed. Data types corrected to floar for unit_sold and unit_price. Ready to test valdation & associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_31_060255) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_31_062846) do
   create_table "grind_types", force: :cascade do |t|
     t.string "grind"
     t.datetime "created_at", null: false
@@ -19,8 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_31_060255) do
 
   create_table "inventories", force: :cascade do |t|
     t.string "item_name"
-    t.string "unit_sold"
-    t.string "unit_price"
+    t.float "unit_sold"
+    t.float "unit_price"
     t.integer "stock_amount"
     t.boolean "new_inventory"
     t.float "total"

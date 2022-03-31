bcc3db5dc69e6d02ab10cb296983aaf50c7b03c8
Kurt Pili <kurtpili3@gmail.com>
2022-03-31 13:54:12 -0500

    Model(inventories): Tests for validations and association to supplier table complete - Minor changes to validation helpers used but overall inline with business functions I eventually will desire by the end of the project.

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -3,12 +3,10 @@ class Inventory < ApplicationRecord
   belongs_to :supplier
 
   # Validation
-  validates :item_name, :unit_sold, :unit_price, :stock_amount, :new_inventory, :total,
+  validates :item_name, :unit_sold, :unit_price, :stock_amount, :total,
             presence: true
-  validates :unit_sold, :unit_price, :stock_amount, :total, numericality: true
   validates :unit_sold, :unit_price, :stock_amount, :total,
             numericality: { greater_than_or_equal_to: 0 }
-  validates :stock_amount, numericality: { only_integer: true }
-  validates :retail_total, numericality: true, comparison: { greater_than: :total },
-allow_blank: true
+  validates :stock_amount, :new_inventory, numericality: { only_integer: true }
+  validates :retail_total, numericality: true, allow_blank: true
 end

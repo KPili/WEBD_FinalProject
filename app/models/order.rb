66768906f651bced382e27363bf9968f9979c70b
Kurt Pili <kurtpili3@gmail.com>
2022-03-31 21:58:55 -0500

    Model(Order): Migrated validation changes, testing of associations and validations complete.

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,8 +6,9 @@ class Order < ApplicationRecord
 
   # Validations
   validates :order_num, :cart_total, presence: true
+  validates :order_num, uniqueness: true
   validates :cart_total, numericality: { greater_than_or_equal_to: 0 }, allow_blank: true
-  validates :email, format: { with: /\A^[\w.\d]+@[\w.\d]+.[comrga]{2,3}$\z/ }
+  validates :email, format: { with: /\A^[\w.\d]+@[\w.\d]+.[comrga]{2,3}$\z/ }, allow_blank: true
   # Email format reference 1: https://www.youtube.com/watch?v=l-Jv5vMjB70
   # Email format reference 2: https://rubular.com/
 end

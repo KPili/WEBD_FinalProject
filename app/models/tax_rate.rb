de3a1db20288448451e72b63bae9c94aacde7804
Kurt Pili <kurtpili3@gmail.com>
2022-03-31 20:27:20 -0500

    Model(tax_rate): Uncommented has_many relatiionship to provinces

--- app/models/tax_rate.rb
+++ app/models/tax_rate.rb
@@ -1,6 +1,6 @@
 class TaxRate < ApplicationRecord
   # Association
-  # has_many :provinces
+  has_many :provinces
 
   # Validation
   validates :gst, :pst, :hst, numericality: true, allow_blank: true

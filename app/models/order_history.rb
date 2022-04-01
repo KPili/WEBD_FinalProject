5fd219d92378d72ebe04b8de96c13c43d4bae9db
Kurt Pili <kurtpili3@gmail.com>
2022-03-31 22:44:08 -0500

    Model(order_history): Adjusted validation to take in blank values for tax rate to match data validation from the tax_rate table

--- app/models/order_history.rb
+++ app/models/order_history.rb
@@ -3,6 +3,7 @@ class OrderHistory < ApplicationRecord
   belongs_to :order
 
   # Validation
-  validates :order_date, :cart_total, :tax_rate, presence: true
+  validates :order_date, :cart_total, presence: true
+  validates :tax_rate, allow_blank: true
   validates :cart_total, numericality: { greater_than_or_equal_to: 0 }
 end

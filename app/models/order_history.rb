682611cfbd1b981f56d85bf08532ea205b3113d6
Kurt Pili <kurtpili3@gmail.com>
2022-03-31 22:26:44 -0500

    Model(Order_history): Migrated validations and testing associations and validations in rails console next.

--- app/models/order_history.rb
+++ app/models/order_history.rb
@@ -1,3 +1,8 @@
 class OrderHistory < ApplicationRecord
+  # Association
   belongs_to :order
+
+  # Validation
+  validates :order_date, :cart_total, :tax_rate, presence: true
+  validates :cart_total, numericality: { greater_than_or_equal_to: 0 }
 end

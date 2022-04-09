59d8625556af34b61375eb61411df986ba6f3ab9
Kurt Pili <kurtpili3@gmail.com>
2022-04-09 11:13:32 -0500

    Model(tax_rate): Added association to join model of province_tax_rate and through it to provinces.

--- app/models/tax_rate.rb
+++ app/models/tax_rate.rb
@@ -1,6 +1,7 @@
 class TaxRate < ApplicationRecord
   # Association
-  has_many :provinces
+  has_many :province_tax_rates
+  has_many :provinces, through: :province_tax_rates
 
   # Validation
   validates :gst, :pst, :hst, :total, numericality: true, allow_blank: true

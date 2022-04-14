f4bb446816fdc54bf92babcc7962e7a121376a5e
Kurt Pili <kurtpili3@gmail.com>
2022-04-14 00:15:22 -0500

    Controller(About): Generated about controller, and the routes to create the appropriate url to the about page of princess coffee roasters website.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  # get 'abouts/index'
   # get 'home/index'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "home#index"
+
+  # About Page
+  get "pcr/about" => "abouts#index", as: "about"
 end

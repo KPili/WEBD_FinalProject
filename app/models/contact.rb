519e2c037dade3bc83159abceeace0385024a51c
Kurt Pili <kurtpili3@gmail.com>
2022-04-13 23:34:25 -0500

    Model(contact): Changed the validation for postal code.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,8 +3,7 @@ class Contact < ApplicationRecord
   validates :street_address, :city, :postal_code, :b_start, :b_end, :s_time, :e_time, :phone,
             :email, presence: true
   # reference: https://stackoverflow.com/questions/19353135/what-is-good-way-to-validates-canadian-postal-code-in-rails
-  validates :postal_code,
-            format: { with: /\A[ABCEGHJKLMNPRSTVXY]{1}\\d{1}[A-Z]{1}[ -]?\\d{1}[A-Z]{1}\\d{1}\z/ }
+  validates :postal_code, length: { maximum: 7 }
   validates :email,
             format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }
 end

ba6e2666d0594b77d628a9486791d1f44756cac8
Kurt Pili <kurtpili3@gmail.com>
2022-04-18 17:49:25 -0500

    Views(Home):Index page styled and prepared for image uploads. Adjusted footer, navbar and pagination count for products.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @inventory_details = InventoryDetail.includes(:inventory).all
+    @inventory_details = Inventory.where("new_inventory == 1").limit(8)
   end
 end

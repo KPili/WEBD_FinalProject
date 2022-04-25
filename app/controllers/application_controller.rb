d6f091f01e95c4d322294ddbf015e074331502a9
Kurt Pili <kurtpili3@gmail.com>
2022-04-25 15:12:23 -0500

    Controller(Application): Added code to allow for session handling for shopping cart.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :initialize_session
+  helper_method :cart
+
+  # Methods for shopping cart using sessions
+
+  private
+
+  # Initialize a new shopping cart
+  def initialize_session
+    session[:shopping_cart] ||= [] # On initial visit, shopping cart is empty
+  end
+
+  # Helper Method to retrieve a collection of products from a collection
+  def cart
+    Inventory.find(session[:shopping_cart])
+  end
 end
